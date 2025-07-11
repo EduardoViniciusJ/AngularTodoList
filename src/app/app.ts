855e29df104633a0321b034d45a269123338a142
Eduardo <eduardoviniciusjorge@gmail.com>
2025-07-11 19:38:08 -0300

    ng for, event click, constructor, array

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
+  public todos: any[] = [];
+  public titleMain: String = 'Tarefas:';
+
+  constructor(){
+    this.todos.push('trabalhar');
+    this.todos.push('estudar');
+    this.todos.push('caminhar');
+    this.todos.push('treinar');
+    this.todos.push('ler');
+  }
+
+  alterarTexto(){
+    this.titleMain = 'Mudando o Título';
+  }
+
   protected readonly title = signal('project-todo');
 }
