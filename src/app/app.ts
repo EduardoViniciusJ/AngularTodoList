eb9bd5eade6dc787b88d1c85cd2076fd7b699e95
Eduardo <eduardoviniciusjorge@gmail.com>
2025-07-12 16:17:57 -0300

    create basic form

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Todo } from '../models/todo.models';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,CommonModule, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -14,8 +15,16 @@ export class App {
 
   public todos: Todo[] = [];
   public titleMain: String = 'Tarefas:';
+  public form: FormGroup; 
 
-  constructor(){
+  constructor(private fb: FormBuilder) {
+  this.form = this.fb.group({
+    title: ['', [  
+      Validators.minLength(3),
+      Validators.maxLength(60),
+      Validators.required
+    ]]
+  });
     this.todos.push(new Todo(1, 'Caminhar', false));
     this.todos.push(new Todo(2, 'Estudar', false));
     this.todos.push(new Todo(3, 'Treinar', true));
