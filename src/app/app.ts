7c5d980c87f1018a11e3d8ab6451800a7a005ad5
Eduardo <eduardoviniciusjorge@gmail.com>
2025-07-13 19:22:53 -0300

    add method save task in local storage

--- src/app/app.ts
+++ src/app/app.ts
@@ -53,7 +53,19 @@ export class App {
     const title = this.form.controls['title'].value;
     const id = this.todos.length + 1;
     this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
   }
+
+  clear(){
+    this.form.reset();
+  }
+
+  save(){
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
 
 
 
